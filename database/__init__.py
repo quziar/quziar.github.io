3e22660c2caa2ab77f728d9d70e5872beda4fe7c
NKiinimy <71762118+NKiinimy@users.noreply.github.com>
2025-03-03 16:09:07 +0800

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,10 +1,12 @@
 import sqlite3
 import os
 
-DB_PATH = r"database\question_bank.db"
+# 使用相對路徑來引用資料庫文件
+DB_PATH = os.path.join(os.path.dirname(__file__), 'database', 'question_bank.db')
 
 class DatabaseConnection:
     def __enter__(self):
+        # 檢查資料庫是否存在
         if not os.path.exists(DB_PATH):
             raise Exception(f"錯誤：資料庫不存在：{DB_PATH}")
         self.conn = sqlite3.connect(DB_PATH)
