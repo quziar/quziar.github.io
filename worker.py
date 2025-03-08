e537ce1cfef49ef5f90a0e20c32bdf2d4aac5f1c
NKiinimy <71762118+NKiinimy@users.noreply.github.com>
2025-03-08 19:31:42 +0800

    Update worker.py

--- worker.py
+++ worker.py
@@ -5,7 +5,7 @@ def sync_databases_to_github():
     while True:
         print("同步資料庫到 GitHub...")
         subprocess.run(["bash", "sync_db_to_github.sh"], check=True)
-        time.sleep(3600)  # 每小時執行一次同步任務
+        time.sleep(60)  # 每分鐘執行一次同步任務
 
 if __name__ == "__main__":
     sync_databases_to_github()
