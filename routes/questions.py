e7f22f84e2808bf60d182aa8c939829b610ad77e
NKiinimy <71762118+NKiinimy@users.noreply.github.com>
2025-03-13 17:29:47 +0800

    Add files via upload

--- routes/questions.py
+++ routes/questions.py
@@ -4,6 +4,7 @@ from scripts.import_questions import import_questions_from_excel
 from scripts.clean_duplicate_questions import clean_duplicate_questions
 from scripts.clear_all_questions import clear_all_questions
 from fastapi.responses import JSONResponse
+from fastapi import File, Form, UploadFile
 import logging
 
 router = APIRouter()
@@ -12,6 +13,7 @@ router = APIRouter()
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+# 查看所有題目
 @router.get("/view_all_questions/")
 async def get_all_questions():
     logger.debug("view_all_questions 路由被觸發")
@@ -21,16 +23,6 @@ async def get_all_questions():
         return JSONResponse(content={"questions": questions}, status_code=200)
     except Exception as e:
         logger.error(f"錯誤詳情: {str(e)}")
-        raise HTTPException(status_code=500, detail=f"無法載入題目，請稍後再試。錯誤詳情: {str(e)}")
-
-# 查看所有題目
-@router.get("/view_all_questions/")
-async def get_all_questions():
-    try:
-        questions = view_all_questions()
-        return JSONResponse(content={"questions": questions}, status_code=200)
-    except Exception as e:
-        # 顯示更多的錯誤細節
         raise HTTPException(status_code=500, detail=f"無法載入題目，請稍後再試。錯誤詳情: {str(e)}")
 
 # 清空所有題目
@@ -44,11 +36,19 @@ async def clear_all():
 
 # 匯入題目
 @router.post("/import-questions/")
-async def import_questions():
+async def import_questions(file: UploadFile = File(...), public_private: str = Form(...)):
     try:
-        import_questions_from_excel()
-        return JSONResponse(content={"message": "題目匯入成功！"}, status_code=201)
+        # 呼叫 import_questions_from_excel 函式並傳入檔案與公私有參數
+        result = import_questions_from_excel(file, public_private)
+        
+        # 根據 import_questions_from_excel 的結果回應
+        if "message" in result:
+            return JSONResponse(content=result, status_code=201)
+        else:
+            raise HTTPException(status_code=500, detail=f"匯入題目時發生錯誤，請稍後再試。錯誤詳情: {result['message']}")
+
     except Exception as e:
+        # 捕捉並處理異常
         raise HTTPException(status_code=500, detail=f"匯入題目時發生錯誤，請稍後再試。錯誤詳情: {str(e)}")
 
 # 整理所有題目
