ba119dd0a3b40117884d5ae0658e4443d774bf5b
NKiinimy <71762118+NKiinimy@users.noreply.github.com>
2025-03-25 05:35:30 +0800

    Add files via upload

--- database/__init__.py
+++ database/__init__.py
@@ -67,7 +67,8 @@ class DatabaseConnection:
                 CREATE TABLE IF NOT EXISTS users (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     username TEXT UNIQUE NOT NULL,
-                    password TEXT NOT NULL
+                    password TEXT NOT NULL,
+                    identities TEXT NOT NULL
                 )
             ''')
             conn.commit()
