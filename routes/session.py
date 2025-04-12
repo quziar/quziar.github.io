ae114c906027da3c2ca9e51f4266f30934ad028c
NKiinimy <71762118+NKiinimy@users.noreply.github.com>
2025-04-12 21:11:03 +0800

    Update session.py

--- routes/session.py
+++ routes/session.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from starlette.requests import Request
 from fastapi.responses import JSONResponse
-from scripts.cookie_session import SECRET_KEY
 
 router = APIRouter()
 
@@ -9,13 +8,11 @@ router = APIRouter()
 async def login(user_id: str, request: Request):
     # 儲存使用者 ID 到 session
     request.state.session["currentUserID"] = user_id
-    print(f"[DEBUG] SECRET_KEY: {SECRET_KEY}")
     return JSONResponse(content={"message": f"使用者 {user_id} 已成功登入"})
 
 @router.get("/get_user/")
 async def get_user(request: Request):
     # 從 session 取得當前使用者
-    print(f"[DEBUG] SECRET_KEY: {SECRET_KEY}")
     current_user = request.state.session.get("currentUserID")
     if current_user is None:
         raise HTTPException(status_code=401, detail="未登入")
