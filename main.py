9127856504bd4deab593e7130cfedfeb3833291e
NKiinimy <71762118+NKiinimy@users.noreply.github.com>
2025-04-12 22:56:22 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import asyncio
 import time
 import subprocess
 import os
+import redis
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 from starlette.middleware.sessions import SessionMiddleware
 from fastapi.middleware.cors import CORSMiddleware
+from scripts.redis_session import RedisSessionMiddleware
 
 # 正確引入路由
 from routes.questions import router as question_router
@@ -16,22 +18,32 @@ from routes.admin import router as admin_router
 from routes.session import router as session_router
 from routes.exams import router as exam_router
 from routes.fonts import router as fonts_router
-from scripts.cookie_session import CookieSessionMiddleware
+
 
 # 初始化 FastAPI 應用
 app = FastAPI(title="題庫系統")
 
 app.add_middleware(
-    CookieSessionMiddleware
-)
-
-app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://quziar-github-io.onrender.com/"],  # 或 ['*'] 測試用
+    allow_origins=["https://www.smartlearningzones.com"],  # 或 ['*'] 測試用
     allow_credentials=True,  # ✅ 重點！讓 cookie 能傳送
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.add_middleware(RedisSessionMiddleware)
+
+# Redis 配置
+redis_url = 'redis://red-cvt7qth5pdvs739hg6o0:6379'  # 使用你自己的 Redis URL
+
+# Redis 連接
+@app.on_event("startup")
+async def startup_event():
+    # 配置 Redis 客戶端
+    app.state.redis = redis.StrictRedis.from_url(redis_url)
+
+    # 在啟動時啟動異步的資料庫同步任務
+    asyncio.create_task(sync_databases_periodically())  # 創建異步任務並啟動
 
 # 根路由重定向到靜態頁面
 @app.get("/", response_class=RedirectResponse)
@@ -61,11 +73,6 @@ async def sync_databases_periodically():
         print("正在同步資料庫...")
         subprocess.run(["bash", "sync_db_to_github.sh"], check=True)
         await asyncio.sleep(36000)  # 每十小時同步一次，這裡使用異步 sleep
-
-@app.on_event("startup")
-async def startup_event():
-    # 在啟動時啟動異步的資料庫同步任務
-    asyncio.create_task(sync_databases_periodically())  # 創建異步任務並啟動
 
 if __name__ == "__main__":
     import uvicorn
