a860df12b5ca636519d88ddaa91a803802b36d46
NKiinimy <71762118+NKiinimy@users.noreply.github.com>
2025-05-07 20:18:25 +0800

    Add files via upload

--- scripts/userlogin.py
+++ scripts/userlogin.py
@@ -5,29 +5,7 @@ from database import USER_DB_PATH, get_user_db
 # 驗證密碼
 def verify_password(stored_hash: str, input_password: str) -> bool:
     return bcrypt.checkpw(input_password.encode(), stored_hash.encode())
-
-# 驗證使用者密碼
-def verify_user_password(db_path, username, input_password):
-    try:
-        with sqlite3.connect(db_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute('SELECT password FROM users WHERE username = ?', (username,))
-            row = cursor.fetchone()
-
-            if row:
-                stored_hashed_password = row[0]
-
-                if verify_password(stored_hashed_password, input_password):
-                    return True
-                else:
-                    return False
-            else:
-                return False
-
-    except Exception as e:
-        print(f"❌ 登入過程中發生錯誤：{str(e)}")
-        return False
-
+    
 def userlogin(username, input_password):
     try:
         with sqlite3.connect(USER_DB_PATH) as conn:
